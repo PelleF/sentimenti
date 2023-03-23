6a366566c3b74f9f792a51ef5b44149de21bff80
huuui828 <48560875+huuui828@users.noreply.github.com>
2023-03-23 14:29:57 +0100

    add recognition

--- BE/Sentimenti/polls/views.py
+++ BE/Sentimenti/polls/views.py
@@ -1,13 +1,14 @@
 # Create your views here.
 from django.http import HttpResponse
 from news.news import NewsApiClass
+from news.news import EntityFilter
 from sentiment.sentiment import SentimentAnalysis
 import json
 
 # Classes instantiated once
 news = NewsApiClass()
 sentiment = SentimentAnalysis()
-
+entity = EntityFilter()
 
 def index(request):
     # Logging
@@ -18,6 +19,9 @@ def index(request):
 
     # Call news API
     list_of_articles = news.get_articles(company_name)
+    
+    # Filter articles based on entity recognition
+    list_of_articles = entity.filter_article(list_of_articles,company_name)
 #
     # Do score
     list_of_articles_sentiments = sentiment.get_list_with_scores(list_of_articles)
